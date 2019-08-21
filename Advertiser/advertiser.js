55c487736bcff5e74f01279bccd8a732900145f9
almills1972 <36703166+almills1972@users.noreply.github.com>
2019-08-21 14:55:43 -0500

    Updated code for Advertiser

--- Advertiser/advertiser.js
+++ Advertiser/advertiser.js
@@ -1,18 +1,23 @@
-const data = require('../class-2-data.json')
+//const data = require('../class-2-data.json')
+const dateTime = require ('moment')
+
+const date = dateTime().format("dddd, MMMM Do YYYY");
 
 function printAdvertiser(littleGirl) {
     
 
-    console.log("==========================================================================================")
-    console.log("| ", `Hi ${littleGirl}`,"                                                                   |")
-    console.log("|                                                                                        |")
-    console.log("|    Would you like to buy your favorite toy?,                                           |")
-    console.log("|          Just to let you know, the sale is ending                                      |")
-    console.log("|                                                                                        |")
-    console.log("|                 Hope to see you soon!                                                  |")
-    console.log("|                 Mills Toy Company                                                      |")
-    console.log("==========================================================================================")
+    console.log("=".repeat(89))
+    console.log("|"," ".repeat(3),`${littleGirl}`," ".repeat(39),"|")
+    console.log("|"," ".repeat(85),"|")
+    console.log("|"," ".repeat(4),"Would you like to buy your favorite toy?,"," ".repeat(38),"|")
+    console.log("|"," ".repeat(4),"Just to let you know, the sale is ending",`${date}`," ".repeat(11),"|")
+    console.log("|"," ".repeat(85),"|")
+    console.log("|"," ".repeat(20),'Hope to see you soon!'," ".repeat(42),"|")
+    console.log("|"," ".repeat(20),'Mills Toy Company'," ".repeat(46),"|")
+    console.log("=".repeat(89))
 }
+
+
 
 
 module.exports = {
--- Advertiser/main.js
+++ Advertiser/main.js
@@ -1,10 +1,13 @@
 const data = require('../class-2-data.json')
 const advertiser = require('./advertiser.js')
+const _ = require('lodash')
 
 const littleGirl = data.map(function(e) {
     if(e.sex === "F" && e.age < 30)
-        return e.name
+        return _.replace(e.name, e.name, `To the little girl named ${e.name}`)             
+    
 })
+
 
 advertiser.printAdvertiser(littleGirl)
 
